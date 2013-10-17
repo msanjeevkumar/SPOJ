c7bcb16f833f0c5d57d7d76847e3f0df10a2f449
Sanjeev Kumar <sanjeev.444456@outlook.com>
2013-10-18 03:27:26 +0530

    problem description and smaple inputs and outputs are added ,link is added

--- test.c
+++ test.c
@@ -1,3 +1,27 @@
+/*
+	http://www.spoj.com/problems/TEST/
+
+
+	1.Life,the Universe,and Everything
+		ProblemCode:TEST
+
+Your program is to use the brute-force approach in order to find the Answer to Life, the Universe, and Everything. More precisely... rewrite small numbers from input to output. Stop processing input after reading in the number 42. All numbers at input are integers of one or two digits.
+
+
+Example
+
+Input:
+1
+2
+88
+42
+99
+
+Output:
+1
+2
+88*/
+
 #include<stdio.h>
 int main()
 {
